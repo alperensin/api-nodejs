ce818804e17b50e504d67ccc5a02bae3716b413f
André Luiz Perensin <andreluizperensin@gmail.com>
2023-02-15 21:23:42 -0300

    First Commit - Create API with nodejs http server

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const http = require("http");
+const { URL } = require("url");
+
+const routes = require("./routes");
+const bodyParser = require("./helpers/bodyParser");
+
+const server = http.createServer((request, response) => {
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`);
+  console.log(
+    `Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`
+  );
+
+  let { pathname } = parsedUrl;
+  let id = null;
+
+  const splitEndpoint = pathname.split("/").filter(Boolean);
+
+  if (splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`;
+    id = splitEndpoint[1];
+  }
+
+  const route = routes.find(
+    ({ method, endpoint }) => method === request.method && endpoint === pathname
+  );
+
+  if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams);
+    request.params = { id };
+    response.send = (statusCode, body) => {
+      response.writeHead(statusCode, { "Content-type": "application/json" });
+      response.end(JSON.stringify(body));
+    };
+
+    if (["POST", "PUT", "PATCH"].includes(request.method)) {
+      bodyParser(request, () => route.handler(request, response));
+    } else {
+      route.handler(request, response);
+    }
+  } else {
+    response.writeHead(404, { "Content-type": "text/html" });
+    response.end(`Cannot ${request.method} ${pathname}`);
+  }
+});
+
+server.listen(3000, () =>
+  console.log("🔥 Server started at http://localhost:3000")
+);
---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: 1,
+    name: "André Luiz",
+  },
+  {
+    id: 2,
+    name: "John Doe",
+  },
+  {
+    id: 3,
+    name: "Aleatory Tester",
+  },
+];
